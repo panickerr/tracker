a13b8639ada08000c575aacc7f552acb44e2911b
Leadics PC11 <Leadics PC11@LAPTOP-7NFBMC62>
2016-12-14 16:53:42 +0530

    unhide modules

--- src/app/module1/module1.module.js
+++ src/app/module1/module1.module.js
@@ -3,9 +3,9 @@
 
     angular
         .module('app.module1', [
-//                'app.module1.page1',
-//                'app.module1.page4',
-//                'app.module1.page5',
+                'app.module1.page1',
+                'app.module1.page4',
+                'app.module1.page5',
                 'app.module1.page6'
           ]);
 })();
